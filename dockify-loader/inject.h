a838ad0061dc79a9b27cf8a601a804dfdc7889e5
Alex Zielenski <alex@zielenski.net>
2014-01-29 17:52:10 -0500

    credit comex

--- dockify-loader/inject.h
+++ dockify-loader/inject.h
@@ -1,6 +1,9 @@
 #include <unistd.h>
 #include <mach/kern_return.h>
 
+// This code is by comex, lifted from https://github.com/comex/inject_and_interpose with a mod on one line
+// I just don't feel like adding a submodule
+
 kern_return_t inject(pid_t pid, const char *path);
 
 // The behavior is synchronous: when it returns, constructors have
